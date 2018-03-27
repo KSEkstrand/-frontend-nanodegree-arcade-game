9bc5fd0903f2da47f959522b5163d5a7721452fe
Kyle Ekstrand <KSEkstrand@gmail.com>
2018-03-26 23:12:03 -0500

    first checkin
    Updated base objects

--- js/app.js
+++ js/app.js
@@ -1,24 +1,86 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+//GameObject Top level object for game
+//1 - has screen location
+//2 - can be touched (i.e. has boundaries)
+//3 - has image
+var GameObject = function(x, y, sprite, dt) {
+  this.x = x;
+  this.y = y;
+  this.sprite = sprite;
+  this.dt = dt;
+  this.width = sprite.width;
+  this.height = sprite.height;
+};
+
+GameObject.prototype.update = function(dt){
+
+};
+
+GameObject.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var CharacterObject = function() {
+  GameObject.call(x, y, sprite);
+};
+
+//player object
+var Player = function(x, y, sprite){
+  //ignore sprite currently, just use standard image obj
+  CharacterObject.call(this, x, y, 'images/char-boy.png');
+};
+
+//function to check boundary of the player object
+//this function only applies to the player, as we don't care if
+//other objects touch
+Player.prototype.checkBoundary = function(gameObject){
+  var boundaryTouched = false;
+  var playerBoundaryXLeft = this.x - this.width/2;
+  var playerBoundaryYBottom = this.y - this.height/2;
+  var playerBoundaryXRight = this.x + this.width/2;
+  var playerBoundaryYTop = this.y + this.height/2;
+
+  var gameObjectBoundaryXLeft = gameObject.x - gameObject.width/2;
+  var gameObjectBoundaryYBottom = gameObject.y - gameObject.height/2;
+  var gameObjectBoundaryXRight = gameObject.x + gameObject.width/2;
+  var gameObjectBoundaryYTop = gameObject.y + gameObject.height/2;
+
+  var xTouch = (playerBoundaryXLeft < gameObjectBoundaryXRight) && (playerBoundaryXRight > gameObjectBoundaryXLeft);
+  var yTouch = (playerBoundaryYBottom < gameObjectBoundaryYTop) && (playerBoundaryYTop > gameObjectBoundaryYBottom);
+
+  if(xTouch && yTouch) {
+    boundaryTouched = true;
+  }
+  return boundaryTouched;
+};
+
+Player.prototype.checkGameEdge = function(x, y){
+  var playerBoundaryXLeft = this.x - this.width/2;
+  var playerBoundaryYBottom = this.y - this.height/2;
+  var playerBoundaryXRight = this.x + this.width/2;
+  var playerBoundaryYTop = this.y + this.height/2;
+
+
+};
+
+Player.prototype.handleInput = function(key){
+  if(key === undefined){
+    console.log("Key not allowed!");
+    return;
+  }
+
+
+};
+
+// Enemies our player must avoid
+var Enemy = function(x, y){
+  CharacterObject.call(this, x, y, 'images/enemy-bug.png');
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  this.x = dt * (x+1);
 };
 
 // Now write your own player class
